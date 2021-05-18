97279b397a6ae0cf632fb9edb2aca7c07b812689
Latha Murugan <81089023+LathaMurugan-code@users.noreply.github.com>
2021-05-18 21:47:54 +0530

    Update ArrayOutofbound.java

--- ArrayOutofbound.java
+++ ArrayOutofbound.java
@@ -6,10 +6,16 @@ public class ArrayOutofbound {
 		// TODO Auto-generated method stub
 		try 
 		{
-		int[] arr = new int[4];
+		int[] arr = new int[5];
+		arr[0]=76;
+		arr[1]=65;
+		arr[2]=84;
+		arr[3]=72;
+		arr[4]=65;
+		
 		for(int i=0;i<5;i++)
 		{
-			arr[i]=5;
+			System.out.print((char)arr[i]+" ");
 		}
 
 	}
